38d41e5e14057adeb6dbad7df67df3c8fa5fae79
Steven Gray <steagray@ut.utm.edu>
2023-02-21 13:03:22 -0600

    high score labels

--- scenes/mainscenes/home.gd
+++ scenes/mainscenes/home.gd
@@ -1,11 +1,20 @@
 extends Node2D
 
+var high_scores = [0, 0]
+
 func _ready():
 	$minigame1.pressed.connect(minigame1)
 	$minigame2.pressed.connect(minigame2)
+	load_scores()
+	var i = 1
+	for score in high_scores:
+		get_node("mg" + str(i) + "hs").text = "High Score: " + str(score)
+
+func load_scores():
+	pass
 
 func minigame1():
-	add_child(load("res://scenes/mainscenes/Minigame1.tscn").instantiate())
+	get_parent().add_child(load("res://scenes/mainscenes/Minigame1.tscn").instantiate())
 
 func minigame2():
-	add_child(load("res://scenes/mainscenes/minigame2.tscn").instantiate())
+	get_parent().add_child(load("res://scenes/mainscenes/minigame2.tscn").instantiate())
